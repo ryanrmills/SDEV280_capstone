b8219bb699a7dc26ce54583a29f0c7937f6538ef
ryanrmills <ryanroymills@gmail.com>
2025-06-07 20:07:11 -0700

    initialized npm and added ai dependencies

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
+
+async function mainAI() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+mainAI();
+
 const urlParams = new URLSearchParams(window.location.search);
 const pdgaNum  = urlParams.get("pdga_number"); //?pdga_number=
 
