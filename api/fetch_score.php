068b1765946e2507f5e3e01f817e8b12e3b38bce
ryanrmills <ryanroymills@gmail.com>
2025-06-14 05:03:47 -0700

    corrected

--- api/fetch_score.php
+++ api/fetch_score.php
@@ -1,5 +1,7 @@
 <?php
-$connection = new mysqli("localhost", "root", "", "sdev280capstone");
+require_once __DIR__ . '/../../../config/db.php';
+
+$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
