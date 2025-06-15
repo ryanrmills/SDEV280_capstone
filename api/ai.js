6a5026f35ba6d99142c26ca3a35fdd022c7582b3
ryanrmills <ryanroymills@gmail.com>
2025-06-15 06:46:35 -0700

    change ai

--- api/ai.js
+++ api/ai.js
@@ -10,11 +10,11 @@ const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 const app = express();
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: "http://localhost",
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:4000"
+  })
+);
 
 
 let aIcontext = '';
